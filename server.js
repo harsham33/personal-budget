48f6adb01f15d49ebae6ce8007f493e22ea5b38e
harsham33 <mulgeharsha03@gmail.com>
2024-09-14 17:58:23 -0400

    Added D3JS chart

--- server.js
+++ server.js
@@ -3,27 +3,28 @@ const app = express();
 const port = 3000;
 
 app.use('/',express.static('public'));
+const budget = require('./budget-data.json');
 
-const budget = {
-    myBudget: [
-    {
-        title: 'Eat out',
-        budget: 25
-    },
-    {
-        title: 'Rent',
-        budget: 375
-    },
-    {
-        title: 'Grocery',
-        budget: 110
-    },
-   ]
-};
-
-app.get('/hello',(req, res) =>{
-    res.send('Hello World!');
-});
+//const budget = {
+//    myBudget: [
+//    {
+//        title: 'Eat out',
+//        budget: 25
+//    },
+//    {
+//        title: 'Rent',
+//        budget: 375
+//    },
+//    {
+//        title: 'Grocery',
+//        budget: 110
+//    },
+//   ]
+//};
+app.use("/", express.static('public'))
+//app.get('/hello',(req, res) =>{
+//    res.send('Hello World!');
+//});
 
 app.get('/budget', (req, res) => {
     res.json(budget);
